649f82dfd8527653b7ef167ea6250bbe78e67ada
410867 <oshchyp123@gmail.com>
2022-10-06 12:54:44 +0300

    add index.html and index.js

---
+++ Scripts/index.js
@@ -0,0 +1,4 @@
+let oneNumber = Number(prompt('Введіть перше число:'));
+let twoNumber = Number(prompt('Введіть друге число:'));
+let sum = oneNumber + twoNumber;
+alert('Сумма  першого та другого чисел: ' + sum);
